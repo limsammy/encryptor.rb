4968becc67425088df5063bb0ee590d07b113a59
Sam Lim <limsammy1@gmail.com>
2017-02-21 10:31:30 -0800

    Fixed decryption method

--- encryptor.rb
+++ encryptor.rb
@@ -20,17 +20,16 @@ class Encryptor
   	results.join
   end
 
-  def decrypt_letter(letter)
-  	lowercase_letter = letter.downcase
-  	reversed_cipher = cipher.to_a.reverse.to_h
-  	reversed_cipher[lowercase_letter]
+  def decrypt_letter(letter, rotation)
+  	cipher_for_rotation = cipher(rotation)
+  	cipher_for_rotation.key(letter)
   end
 
-  def decrypt(string)
+  def decrypt(string, rotation)
   	letters = string.split("")
 
   	results = letters.collect do |letter|
-  		decrypt_letter = decrypt_letter(letter)
+  		decrypt_letter = decrypt_letter(letter, rotation)
   	end
 
   	results.join
