9d64043e2d76fdd4b2cf304d73685a7ac8b7db3c
Sam Lim <limsammy1@gmail.com>
2017-02-21 06:00:52 -0800

    switched to .collect method. TODO: add support for spaces

--- encryptor.rb
+++ encryptor.rb
@@ -9,8 +9,21 @@ class Encryptor
      'y' => 'l', 'z' => 'm'}
   end
 
-  def encrypt(letter)
+  def encrypt_letter(letter)
   	lowercase_letter = letter.downcase
   	cipher[lowercase_letter]
+  end
+
+  def encrypt(string)
+  	# cut string into letters array
+  	letters = string.split("")
+
+  	# encrypt letters one at a time, add encrypted letter to results array
+  	results = letters.collect do |letter|
+  		encrypt_letter = encrypt_letter(letter)
+  	end
+
+  	# join the results into one, fully encrypted, string
+  	results.join
   end
 end
