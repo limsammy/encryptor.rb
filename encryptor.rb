2a10fd0f26724897e32b1b024b6f2fe186ae6d03
Sam Lim <limsammy1@gmail.com>
2017-02-21 06:33:35 -0800

    added functionality for full rotation for cipher

--- encryptor.rb
+++ encryptor.rb
@@ -1,29 +1,22 @@
 class Encryptor
-  def cipher
-    {'a' => 'n', 'b' => 'o', 'c' => 'p', 'd' => 'q',
-     'e' => 'r', 'f' => 's', 'g' => 't', 'h' => 'u',
-     'i' => 'v', 'j' => 'w', 'k' => 'x', 'l' => 'y',
-     'm' => 'z', 'n' => 'a', 'o' => 'b', 'p' => 'c',
-     'q' => 'd', 'r' => 'e', 's' => 'f', 't' => 'g',
-     'u' => 'h', 'v' => 'i', 'w' => 'j', 'x' => 'k',
-     'y' => 'l', 'z' => 'm'}
+  def cipher(rotation)
+  	characters = (' '..'z').to_a
+  	rotated_characters = characters.rotate(rotation)
+  	Hash[characters.zip(rotated_characters)]
   end
 
-  def encrypt_letter(letter)
-  	lowercase_letter = letter.downcase
-  	cipher[lowercase_letter]
+  def encrypt_letter(letter, rotation)
+  	cipher_for_rotation = cipher(rotation)
+  	cipher_for_rotation[letter]
   end
 
-  def encrypt(string)
-  	# cut string into letters array
+  def encrypt(string, rotation)
   	letters = string.split("")
 
-  	# encrypt letters one at a time, add encrypted letter to results array
   	results = letters.collect do |letter|
-  		encrypt_letter = encrypt_letter(letter)
+  		encrypt_letter = encrypt_letter(letter, rotation)
   	end
 
-  	# join the results into one, fully encrypted, string
   	results.join
   end
 
