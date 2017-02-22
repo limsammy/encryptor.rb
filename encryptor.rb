27661b01d870fb853162def9f45fc2bfe7651b17
Sam Lim <limsammy1@gmail.com>
2017-02-21 21:05:46 -0800

    Added functionality for 3 rotations to be used

--- encryptor.rb
+++ encryptor.rb
@@ -9,47 +9,55 @@ class Encryptor
 		Hash[supported_characters.zip(rotated_characters)]
 	end
 
-	def encrypt_letter(letter, rotation)
-		cipher_for_rotation = cipher(rotation)
-		cipher_for_rotation[letter]
+	def encrypt_letter(letter, rotation1, rotation2, rotation3)
+		cipher_for_rotation1 = cipher(rotation1)
+		cipher_for_rotation1[letter]
+		cipher_for_rotation2 = cipher(rotation2)
+		cipher_for_rotation2[letter]
+		cipher_for_rotation3 = cipher(rotation3)
+		cipher_for_rotation3[letter]
 	end
 
- 	def encrypt(string, rotation)
+ 	def encrypt(string, rotation1, rotation2, rotation3)
  		letters = string.split("")
 
  		letters.collect do |letter|
- 			encrypt_letter(letter, rotation)
+ 			encrypt_letter(letter, rotation1, rotation2, rotation3)
  		end.join()
  	end
 
- 	def decrypt_letter(letter, rotation)
- 		decipher = cipher(rotation).invert
- 		decipher[letter]
+ 	def decrypt_letter(letter, rotation1, rotation2, rotation3)
+ 		decipher1 = cipher(rotation1).invert
+ 		decipher1[letter]
+ 		decipher2 = cipher(rotation2).invert
+ 		decipher2[letter]
+ 		decipher3 = cipher(rotation3).invert
+ 		decipher3[letter]
  	end
 
- 	def decrypt(string, rotation)
+ 	def decrypt(string, rotation1, rotation2, rotation3)
  		letters = string.split("")
 
  		letters.collect do |letter|
- 			decrypt_letter(letter, rotation)
+ 			decrypt_letter(letter, rotation1, rotation2, rotation3)
  		end.join()
  	end
 
  	# Outputing to text files
- 	def encrypt_file(filename, rotation)
+ 	def encrypt_file(filename, rotation1, rotation2, rotation3)
  		input = File.open(filename, "r")
  		message = input.read
- 		encrypted_message = encrypt(message, rotation)
+ 		encrypted_message = encrypt(message, rotation1, rotation2, rotation3)
  		output_filename = filename.gsub(".txt", ".txt.encrypted")
  		output = File.open(output_filename, "w")
  		output.write(encrypted_message)
  		output.close
  	end
 
- 	def decrypt_file(filename, rotation)
+ 	def decrypt_file(filename, rotation1, rotation2, rotation3)
  		input = File.open(filename, "r")
  		message = input.read
- 		decrypted_message = decrypt(message, rotation)
+ 		decrypted_message = decrypt(message, rotation1, rotation2, rotation3)
  		output_filename = filename.gsub("encrypted", "decrypted")
 		output = File.open(output_filename, "w")
 		output.write(decrypted_message)
